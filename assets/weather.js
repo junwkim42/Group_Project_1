fb8be73d8aa385643805ee6942578da861961cb2
Scott Kim <scottkim1003@gmail.com>
2019-07-23 12:53:12 -0400

    map added

--- assets/weather.js
+++ assets/weather.js
@@ -2,6 +2,7 @@
 
 var APIKey = "d1c9275e14261ab240fbf7eb6420e249";
 
+
     // Choose whether to call by city name or coordinates
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
       "q=Toronto,Canada&units=metric&appid=" + APIKey;
@@ -19,7 +20,10 @@ var APIKey = "d1c9275e14261ab240fbf7eb6420e249";
     })
       // We store all of the retrieved data inside of an object called "response"
       .then(function(response) {
+        var iconCode = response.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
 
+        
         // Log the queryURL
         console.log(queryURL);
 
@@ -32,7 +36,7 @@ var APIKey = "d1c9275e14261ab240fbf7eb6420e249";
         $(".humidity").text("Humidity: " + response.main.humidity);
         $(".temp").text("Temperature (C) " + response.main.temp);
         $(".description").text("Weather Outlook:" + response.weather[0].description);
-		$(".icon").html("<img src=" + response.weather[0].icon + ">");
+		$(".icon").html("<img src=" + iconURL + ">");
         console.log(response.weather[0].icon );
         // Log the data in the console as well
         console.log("Wind Speed: " + response.wind.speed);
